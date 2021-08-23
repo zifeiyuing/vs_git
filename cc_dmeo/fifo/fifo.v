68fb98ec3ecdc3b8ba3396856eb197ba0300edf8
zifeiyuing <18229046371fxl@sina.com>
2021-08-23 21:47:35 +0800

    master

---
+++ cc_dmeo/fifo/fifo.v
@@ -0,0 +1,67 @@
+module    fifo #(parameter     data_width = 8,
+                               data_depth = 16,
+                               addr_width = 4)
+(   input    wire                   clk,
+    input    wire                   rst_n,
+    input    wire                   wr_en,
+    input    wire[data_width-1:0]   wr_data,
+    input    wire                   rd_en,
+    output   reg [data_width-1:0]   rd_data,
+    output   reg                    full,
+    output   reg                    empty
+);
+//addr
+reg[addr_width-1:0]        wr_addr,rd_addr;
+//memory
+reg[data_width-1:0]        mem[data_depth-1:0];
+//counter
+reg[addr_width-1:0]        count;
+
+wire                    rd_allow = rd_en && !empty;
+wire                    wr_allow = wr_en && !full;
+
+
+//empty
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
+        empty <= 1'b1;
+    else
+        empty <= (!wr_en && count[addr_width-1:1]=='b0) && (count[0]==1'b0 || rd_en);
+
+//full
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
+        full <= 1'b0;
+    else
+        full <= (!rd_en && count[addr_width-1:1]=={addr_width-1{1'b1}})  && (count[0]==1'b1 || wr_en);
+
+//rd_addr
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
+        rd_addr <= 'd0;
+    else if(rd_allow) begin
+        rd_data <= mem[rd_addr];
+        rd_addr <= rd_addr + 1'b1;
+    end
+
+
+//wr_addr
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
+        wr_addr <= 'd0;
+    else if(wr_allow) begin
+        mem[wr_addr] <= wr_data;
+        wr_addr <= wr_addr + 1'b1;
+    end
+
+always @(posedge clk or negedge rst_n)
+    if (!rst_n)
+        count <= 'd0;
+    else if ( (!rd_allow && wr_allow) || (rd_allow && !wr_allow) ) begin
+        if( wr_allow )
+            count <= count + 1'b1;
+        else
+            count <= count - 1'b1;
+    end
+
+endmodule
---
+++ cc_dmeo/fifo/fifo_tb.v
@@ -0,0 +1,77 @@
+`timescale 1ns/1ps
+module fifo_tb ();
+
+parameter    data_width = 8,
+             data_depth = 16,
+             addr_width = 4,
+             PERIOD   = 20;
+reg                     clk,rst_n,wr_en,rd_en;
+reg [data_width-1:0]    wr_data;
+wire                    full,empty;
+wire [data_width-1:0]   rd_data;
+
+fifo #(.data_width(data_width),
+            .data_depth(data_depth),
+            .addr_width(addr_width)
+       )
+fifo_inst(
+    .clk (clk ),
+    .rst_n (rst_n ),
+    .wr_en (wr_en ),
+    .wr_data(wr_data),
+    .rd_en (rd_en ),
+    .rd_data(rd_data),
+    .full(full  ),
+    .empty(empty)
+);
+
+initial begin
+    rst_n = 0;
+    clk = 1;
+    #5
+    rst_n = 1;
+end
+
+always #(PERIOD/2)    clk=~clk;
+
+initial begin
+    wr_en = 1;
+    #20    wr_data = 8'd0;
+    #20    wr_data = 8'd1;
+    #20    wr_data = 8'd2;
+    #20    wr_data = 8'd3;
+    #20    wr_data = 8'd4;
+    #20    wr_data = 8'd5;
+    #20    wr_data = 8'd6;
+    #20    wr_data = 8'd7;
+    #20    wr_data = 8'd8;
+    #20    wr_data = 8'd9;
+ 
+	#20
+    rd_en = 1;
+    wr_en = 0;
+	#20
+    rd_en = 1;
+    wr_en = 0;
+	#20
+    rd_en = 1;
+    wr_en = 0;
+	#20  
+    rd_en = 0;
+    wr_en = 1;
+    wr_data = 8'd88;
+	#20    wr_data = 8'd11;
+	#20    wr_data = 8'd12;
+	#20 
+	rd_en = 1;
+    wr_en = 0;
+	#20
+    rd_en = 1;
+    wr_en = 1;
+	wr_data = 8'd33;
+	#20
+	wr_en = 1;
+	rd_en = 1;
+	wr_data = 8'd33;
+end
+endmodule
